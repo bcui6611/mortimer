ba65851804b1d5aa2f0d31946e7f7fcd07c0db9f
Aaron Miller <apage43@ninjawhale.com>
2013-06-29 15:41:06 -0700

    Only pull in incanter.core, not all of incanter
    reduces uberjar size by a good 20 meg. Could still be smaller though.
    
    cbugg: closes bug-663

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :main mortimer.web
   :plugins [[lein-marginalia "0.7.1"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [incanter "1.5.1"]
+                 [incanter/incanter-core "1.5.1"]
                  [aleph "0.3.0-rc1"]
                  [cheshire "5.2.0"]
                  [compojure "1.1.5"]
